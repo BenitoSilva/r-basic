cae1c47ca57a0c8c3eafeb1e6599a31fb74027db
BenitoSilva <BenitoSilva@users.noreply.github.com>
2020-06-04 18:07:38 -0500

    Update

---
+++ Mis Scripts/001 R.R
@@ -0,0 +1,73 @@
+# Mi primer Script
+# ALT ENTER Para ejecutar el Script
+
+# R como Calculadora (Científica)
+
+
+# Suma - Resta
+2+2
+
+4-3
+
+# Multiplicación - División
+3*4
+6/2
+7/3
+
+# Elevar a x...
+2^5
+3^13
+
+# Cociente división entero... %/%
+7%/%3
+
+# Resto división entero %%
+7%%3
+
+
+# Ejemplo: ¿Cuántas semanas hay en 725 días? ¿Cuántos días sobran?
+# D = d * q + r
+# r = D - d * q
+# q = D %/% d
+# x = D %% d
+
+725%/%7
+
+725%%7
+
+# Pi
+pi 
+
+# Tau
+2 * pi
+
+
+# Inf
+
+Inf
+
+-Inf
+
+
+# NaN = Indeterminación (Not a Number). NA = Valor desconocido (Not Available)
+
+NA
+
+NaN
+
+5/0
+
+0/0
+
+
+# Números Grandes-Pequeños. (Notación científica)
+
+2^50 # 1.1259e+15
+
+2^(-15) # 3.051758e-05
+
+# Vector
+
+c(2^30, 2^-15, 1, 2, 3/2)
+
+
